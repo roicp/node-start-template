9f1e704d2b1216f606ce9b665b00e94f7def4c69
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2020-05-07 22:13:52 -0300

    Install Express

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  return res.status(200).json({ message: 'Welcome to Express API template' });
+});
+
+module.exports = router;
