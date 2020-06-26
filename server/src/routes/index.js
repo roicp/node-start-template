aebc861c5f7c997738ef03bcae2e2eab37e72a3d
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2020-06-26 14:33:12 -0300

    dot env

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
-});
+router.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default router;
