c62b1c9a28521a58a6b748aba02db6d18f1e4d30
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2020-06-26 13:49:20 -0300

    clean

--- server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { testEnvironmentVariable } from '../settings';
-
-const router = express.Router();
-
-router.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
-
-export default router;
--- server/src/settings.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
-
-export default {
-  testEnvironmentVariable
-};
