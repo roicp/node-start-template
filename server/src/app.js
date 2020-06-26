c30911a6eecc3c5b7623399f32e71f5963cfd455
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2020-06-26 13:44:48 -0300

    .

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/v1', indexRouter);
+
+export default app;
---
+++ server/src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import { testEnvironmentVariable } from '../settings';
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
+
+export default router;
